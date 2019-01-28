59317a2c98e11014e071b6e90b32de2f8b7481cb
Justin Francos <jfrancos@mit.edu>
2019-01-27 23:47:04 -0500

    Updated some variable names and tweaked some pexpect timing

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 prompt = 'moira:  '
-file = open("../2019-01-23-active_IPs.csv", "r") # Ask for file
+file = open("../2019-01-25-active_IPs.csv", "r") # Ask for file
 
 MACtable = []
 for line in file:
@@ -23,32 +23,32 @@ moira.expect(prompt)
 matches = 0
 conflicts = 0
 
-for i, line in enumerate(MACtable):
-	print(f"\r{line[2]} {i}/{len(MACtable)}: {conflicts} conflicts out of {matches} matches", file=sys.stderr, end='', flush=True)
-	moira.sendline("qy ghid HWADDR " + line[2] )
+for i, row in enumerate(MACtable):
+	moira.sendline("qy ghid HWADDR " + row[2] )
+	print(f"\r{row[2]} {i}/{len(MACtable)}: {conflicts} conflicts out of {matches} matches", file=sys.stderr, end='', flush=True)
 	moira.expect(prompt)
 	search = re.search('([\w-]+).MIT.EDU', moira.before)
 	if search:
 		matches += 1
-		line += [search[0], search[1]]
-		if line[1] != line[5]:
+		row += [search[0], search[1]]
+		if row[1] != row[5]:
 			conflicts += 1
-			moira.sendline("qy ghus " + line[4])
+			moira.sendline("qy ghus " + row[4])
 			moira.expect(prompt)
 			search = re.search('HOSTADDRESS, ([\d.]*)', moira.before)
-			line[4] = search[1]
+			row[4] = search[1]
 
 conflict_text = "\n"
 agreement_text = "\nThe following hosts have MAC entries in Moira that agree with the ones in Netregadmin:\n"
 
-MACtable = [line for line in MACtable if len(line) > 4]
-for entry in MACtable:
-	if entry[1] != entry[5]:
-		conflict_text += f"\n{entry[2]}, network {entry[3]}:\n"
-		conflict_text += f"      Moira DHCP will offer {entry[4]} ({entry[5]}.mit.edu)\n"
-		conflict_text += f"Netregadmin DHCP will offer {entry[0]} ({entry[1]}.mit.edu)\n"
+MACtable = [row for row in MACtable if len(row) > 4]
+for row in MACtable:
+	if row[1] != row[5]:
+		conflict_text += f"\n{row[2]}, network {row[3]}:\n"
+		conflict_text += f"      Moira DHCP will offer {row[4]} ({row[5]}.mit.edu)\n"
+		conflict_text += f"Netregadmin DHCP will offer {row[0]} ({row[1]}.mit.edu)\n"
 	else:
-		agreement_text += f"{entry[0]} ({entry[1]}.mit.edu)\n"
+		agreement_text += f"{row[0]} ({row[1]}.mit.edu)\n"
 
 print("", file=sys.stderr)
 print(f"\r{conflicts} conflicts out of {matches} matches                               ")
